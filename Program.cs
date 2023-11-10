70773a44c9c52a48fc5e7c505de1971ba419ad85
isalmanis <intars.salmanis@va.lv>
2023-11-11 01:27:48 +0200

    added settings load from file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿using Umbrella;
+﻿using Microsoft.Extensions.Configuration;
+using Umbrella;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("local.settings.json", true, true)
+            .Build();
+        
         var app = new Application();
         app.Run();
         
