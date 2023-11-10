44b5d6141259b4c4d711384203d0c0a5d6f5ba3c
isalmanis <intars.salmanis@va.lv>
2023-11-11 01:42:24 +0200

    logger added

--- Program.cs
+++ Program.cs
@@ -1,18 +1,28 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Umbrella;
+using Umbrella.Services;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var config = new ConfigurationBuilder()
-            .AddJsonFile("local.settings.json", true, true)
-            .Build();
-        
-        var app = new Application();
-        app.Run();
-        
-        Console.WriteLine("Exiting the program...");
-        Console.ReadKey(); // Keep the console window open
+        try
+        {
+            var logger = new SimpleLogger();
+            logger.LogInfo("Starting...");
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("local.settings.json", true, true)
+                .Build();
+            logger.LogInfo("Config built");
+            var app = new Application();
+            app.Run();
+            logger.LogInfo("Exiting the program...");
+            Console.ReadKey(); // Keep the console window open
+        }
+        catch (Exception e)
+        {
+            
+        }
+
     }
 }
