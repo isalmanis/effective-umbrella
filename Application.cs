9e917d90de7885c84bc3884869464fc65e8f3bbb
isalmanis <intars.salmanis@va.lv>
2023-11-11 00:39:00 +0200

    missing init files

--- Application.cs
+++ Application.cs
@@ -2,5 +2,26 @@
 
 public class Application
 {
-    
+    public void Run()
+    {
+        var exitRequested = false;
+
+        while (!exitRequested)
+        {
+            Console.WriteLine("Main Menu:");
+            Console.WriteLine("0. Exit");
+
+            string? mainChoice = Console.ReadLine();
+
+            switch (mainChoice)
+            {
+                case "0":
+                    exitRequested = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please select again.");
+                    break;
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Umbrella;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var app = new Application();
+        app.Run();
+        
+        Console.WriteLine("Exiting the program...");
+        Console.ReadKey(); // Keep the console window open
+    }
+}
