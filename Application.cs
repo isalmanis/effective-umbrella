76a428f404046bbdafbfa5703b79c04f04ffcbab
isalmanis <intars.salmanis@va.lv>
2023-11-11 01:48:58 +0200

    release poc

--- Application.cs
+++ Application.cs
@@ -9,12 +9,26 @@ public class Application
         while (!exitRequested)
         {
             Console.WriteLine("Main Menu:");
+            
+            Console.WriteLine("1. See all upcoming");
+            Console.WriteLine("2. See all");
+            Console.WriteLine("3. Search");
+            Console.WriteLine("9. Integrity check");
+            
             Console.WriteLine("0. Exit");
 
             string? mainChoice = Console.ReadLine();
 
             switch (mainChoice)
             {
+                case "1":
+                    Console.WriteLine("upcoming...");
+                    Task.Delay(500);
+                    break;
+                case "2":
+                    Console.WriteLine("upcoming...");
+                    Task.Delay(500);
+                    break;
                 case "0":
                     exitRequested = true;
                     break;
