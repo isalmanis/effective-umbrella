f0e4833c78a974512829ef7b2cd03fb0abe6598d
isalmanis <intars.salmanis@va.lv>
2024-01-13 14:26:11 +0200

    another pi precision increase

--- PiHelper.cs
+++ PiHelper.cs
@@ -4,6 +4,6 @@ namespace Umbrella;
 
 public static class PiHelper
 {
-    // Take 3. into account precision is now 8
-    public static string GetPi() => Math.PI.ToString(CultureInfo.InvariantCulture)[..12];
+    // Take 3. into account precision is now 12
+    public static string GetPi() => Math.PI.ToString(CultureInfo.InvariantCulture)[..14];
 }
